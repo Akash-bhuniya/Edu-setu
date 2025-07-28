0cb9ce1c157c094575bbabbf65864fdc0d86b864
Akash-bhuniya <akashbhuniya95@gmail.com>
2025-07-28 20:53:13 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+// Basic Service Worker for caching files
+const CACHE_NAME = 'edu-setu-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './assets/logo-192.png',
+  './assets/logo-512.png'
+];
+
+// Install
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) =>
+      response || fetch(event.request)
+    )
+  );
+});
